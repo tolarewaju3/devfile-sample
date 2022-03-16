04242f279ca70353b7f17a34251e214bc9d79df7
TiOluwa Olarewaju <tolarewa@redhat.com>
2022-03-16 09:50:37 -0500

    update js file

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', (req, res) => {
   var env = process.env;
 
   Object.keys(env).forEach(function(key) {
-    if (env[key] && !key.startsWith('npm_')){
+    if (env[key] && !key.startsWith('npm')){
         text = text + key + '=' + env[key] + '<br>';
     }
   });
